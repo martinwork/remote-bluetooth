e1d7fbe0f1ae5038d4baab54ecd953dc7e53be8f
Martin Williams <martinwork@users.noreply.github.com>
2025-12-19 20:45:56 +0000

    Update test.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
-// Add your code here
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Sad)
+})
+remote.onLine(function (line) {
+    remote.sendString("echo " + line)
+})
+remote.useSerial()
+remote.useBluetooth()
+basic.showIcon(IconNames.Heart)
